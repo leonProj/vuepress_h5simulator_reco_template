19cfd496b1c765d6ed4fb5f8a07212f340582c0e
DESKTOP-3VSNS1I\leon <501646203@qq.com>
2022-08-23 15:22:09 +0800

    fix enchanceApp

---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+
+// 使用异步函数也是可以的
+export default ({
+    Vue, // VuePress 正在使用的 Vue 构造函数
+}) => {
+    Vue.mixin({
+        mounted() {
+            // 判断某一个页面是否需要添加右边预览的右内边距，通过一个类名实现，
+            // 此for-simulator-padding类名定义于/docs/.vuepress/styles/index.styl
+            if (this.$page && this.$page.path) {
+                let pageRoot = this.$page.path.split(".")[0];
+                if (this.$themeConfig.simulatorUrl[pageRoot]) {
+                    try {
+                        document.getElementById('app').classList.add("for-simulator-padding");
+                    } catch (e) {
+                        //TODO handle the exception
+                    }
+                } else {
+                    try {
+                        document.getElementById('app').classList.remove("for-simulator-padding");
+                    } catch (e) {
+                        //TODO handle the exception
+                    }
+                }
+            }
+        }
+    })
+}
