c2911b016ce1101c0742ac952e3d15e2d0912aec
DESKTOP-3VSNS1I\leon <501646203@qq.com>
2022-08-24 11:05:02 +0800

    打包 & 打包时document 未找到的问题

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -23,17 +23,17 @@ export default ({
                     }
                 }
             }
+            // 锚点跳转
+            router.onReady(() => {
+                const { hash } = document.location;
+                setTimeout(() => {
+                    if (hash.length > 1) {
+                        const id = decodeURIComponent(hash);
+                        const el = document.querySelector(`.reco-side-${decodeURIComponent(id).substring(1)}`);
+                        el.click();
+                    }
+                }, 500);
+            });
         }
     })
-    // 锚点跳转
-    router.onReady(() => {
-        const { hash } = document.location;
-        setTimeout(() => {
-            if (hash.length > 1) {
-                const id = decodeURIComponent(hash);
-                const el = document.querySelector(`.reco-side-${decodeURIComponent(id).substring(1)}`);
-                el.click();
-            }
-        }, 500);
-    });
 }
